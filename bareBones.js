bb4070df64415765e54cb9f26b3f6a92191015c3
Martha Kelly <mk.girdler@gmail.com>
2012-06-14 14:41:17 -0400

    naive first approach at formatting my syntax to valid css

--- bareBones.js
+++ bareBones.js
@@ -5,12 +5,39 @@ var source = process.argv.splice(2)[0];
 var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 
 fs.readFile(source, 'utf-8', function(err, data) {
-// utils
-	// something that strips white space
-var output = data;
+
+var dataFormatter = function (data) {
+	return data.map(function(line) {
+		return {
+			line: line
+		};
+	});
+};
+
+var treeFormatter = function (data) {
+	return data.map(function(elem) {
+		if (elem.line.charAt(0) == " ") {
+			return elem.line + ";";
+		} else if (elem.line == "") {
+			return elem.line + "}";
+		} else {
+			return elem.line + " {";
+		}
+	});
+};
+
+var parts = data.split('\n');
+
+var tree = dataFormatter(parts);
+
+var join = treeFormatter(tree).join('\n');
+
+var final = join.charAt(join.length-1) === "}" ? join : join + "\n}";
+
+var output = final;
 
 fs.writeFile(target, output, function(err) {
         if (err) throw err;
-        console.log('Wrote ' + target + '!');
+        console.log('Transpiled ' + source + ' to ' + target);
     });
 });
