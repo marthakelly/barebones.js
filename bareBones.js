fdaa94f4b1e7258e85f7f685ff8fa788125bcb21
Martha Kelly <mk.girdler@gmail.com>
2012-06-18 11:11:38 -0400

    OMG ALMOST GOT VARIABLES WORKING

--- bareBones.js
+++ bareBones.js
@@ -47,9 +47,8 @@ fs.readFile(bare, 'utf-8', function(err, data) {
 				
 		data.map(function(elem) {
 			var parent = tree.length-1;
-			
 			if (elem.variable) {
-				variables.push({variable: elem.variable});
+				variables.push(elem.variable.split('='));
 			} else if (elem.selector) {
 				tree.push({selector: elem.selector, declarations: []});
 			} else if (elem.children != "") {
@@ -57,6 +56,30 @@ fs.readFile(bare, 'utf-8', function(err, data) {
 				tree[parent].children = {selector: childSelector, declarations: []};
 			} else if (elem.declaration.length >= 1) {
 				var value = elem.declaration.toString();
+				
+				
+				for (var i = 0, i; i < variables.length; i++) {
+					var one = variables[i][0].trim(),
+						two = variables[i][1].trim();
+					
+					value = value.replace(one, two);
+					// console.log(variables[i][0]);
+					// console.log(variables[i][1]);
+					// console.log(variables);
+				}
+				
+				console.log(value);
+				
+				/*variables.forEach(function(elem){
+					var one = elem[0],
+						two = elem[1];
+						// console.log(one);
+						// console.log(two);
+						//var newValue = value.replace(one, two);
+						//console.log(newValue);
+						console.log(elem);
+				});*/
+				
 				if (tree[parent].children) {
 					tree[parent].children.declarations.push(value);
 				} else {
@@ -64,28 +87,37 @@ fs.readFile(bare, 'utf-8', function(err, data) {
 				}
 			}
 		});
+				
+		/*variables.map(function(elem){			
+			var variable = elem[0],
+				value = elem[1];
+				
+				// console.log(variable);
+				// console.log(value);
+		});*/
+		
+		// console.log(variables);
+
 		return tree;
 	};
 	
 	var display = function (tree) {
-
 		return tree.map(function(elem){
 			return elem.selector + " {" + "\n" + elem.declarations.join("; \n") + ';' + '\n' + '}';
-			//console.log(elem.selector + " {" + "\n" + elem.declarations.join("; \n") + ';' + '\n' + '}');
-		});
-		
+		});	
 	};
 	
-	// stringify console.log(JSON.stringify(cssFormatter(dataFormatter(data.split('\n'))), undefined, 2));
+	// stringify
+	// console.log(JSON.stringify(cssFormatter(dataFormatter(data.split('\n'))), undefined, 2));
+	
 	var dataFormatter = dataFormatter(data.split('\n'));
 
-	console.log(display(cssFormatter(dataFormatter)));
+	// console.log(display(cssFormatter(dataFormatter)));
 	
 	var output = (display(cssFormatter(dataFormatter))).join('\n');
 
 	fs.writeFile(css, output, function(err) {
 		if (err) throw err;
-		
 		console.log('Converted ' + bare + ' to ' + css);
     });
 });
